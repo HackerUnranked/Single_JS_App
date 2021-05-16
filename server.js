de866e488701343ba391f3fa88bd01fb89f59f86
unknown <cloudiebro@hotmail.com>
2021-05-15 20:18:53 -0700

    11 min and 30 seconds timestamp commit added index js file

--- server.js
+++ server.js
@@ -1,9 +1,24 @@
+// This is how we import modules in JS
 const express = require("express");
 const path = require("path");
 
+// the app is an express app
 const app = express();
 
+// use vs get
+// app.get is called when the http method is a get
+// app.use is called whatever the http method is
+app.use("/static", express.static(path.resolve(__dirname, "frontend", "static")))
+
+// if we get any links we will just look at index.html in the frontend folder
+// req us a request from http and res is the response to send back
 app.get("/*", (req, res) => {
 
+    // we send a file to as a response to render i believe
+    // path.resolve pre-appends from right to left to find the absolute path of the file
+    // example index.html is first then we add frontend to get the path ../frontend/index.html
     res.sendFile(path.resolve("frontend", "index.html"))
 })
+
+// listen to a port and display the message
+app.listen(process.env.PORT || 8000, () => console.log("Server running just like this"));
