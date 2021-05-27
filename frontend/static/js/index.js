6b07d596e9a87b48eced889edf687a9487a72153
unknown <cloudiebro@hotmail.com>
2021-05-26 20:27:19 -0700

    finished doing the application

--- frontend/static/js/index.js
+++ frontend/static/js/index.js
@@ -1,3 +1,7 @@
+import Dashboard from "./views/Dashboard.js";
+import Posts from "./views/Posts.js";
+import Settings from "./views/Settings.js";
+
 // If you expect the back-button to take you to the previous piece of content you interacted 
 // with on the page that is the only case for using history.pushState() since it adds a new history entry.
 const navigate_to = url => {
@@ -11,9 +15,9 @@ const router = async () => {
     // this is an array of objects in js
     const routes = [
 
-        {path: "/", view: () => console.log("Viewing Dashboard") },
-        {path: "/posts", view: () => console.log("Viewing Posts") },
-        {path: "/settings", view: () => console.log("Viewing Settings") },
+        {path: "/", view: Dashboard },
+        {path: "/posts", view: Posts },
+        {path: "/settings", view: Settings },
     ];
 
     // this calls the route function on every obj in routes
@@ -29,8 +33,10 @@ const router = async () => {
         }; 
     });
 
+    // find if the location is a match
     let matches = potentialMatches.find(potentialMatch => potentialMatch.isMatch);
 
+    // if there are no matches go to the first route
     if (!matches)
     {
         matches = {
@@ -39,11 +45,17 @@ const router = async () => {
         };
     }
 
-    // display the obj
-    console.log(matches.route.view());
+    // create a new class object
+    const view = new matches.route.view();
+
+    // grab the app element
+    document.querySelector("#app").innerHTML = await view.getHtml();
+ 
 };
 
-window.addEventListener("popstate", router)
+// listen when a history changes because of the browser action call router
+// an example is pressing the back button on the browser
+window.addEventListener("popstate", router);
 
 // when the document is loaded call router function
 document.addEventListener("DOMContentLoaded", () => {
